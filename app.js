0cf08f4d10c41b9e430cb28c1016248b7ed42af7
dragancodesign <dragan.codesign@gmail.com>
2019-03-05 19:47:55 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// SECTION 16 jQuery Project - News of the Day App 
+
+// * * * 80 News of the Day App * * * 
+
+// * * * 81 Preparing HTML markup * * * 
+
+// * * * 82 Adding - Preparing CSS Styling * * * 
+
+// * * * 83 Making AJAX call to API and getting random item * * * NOT WORKED
+// GET THE: floor value (low)
+// var f = Math.floor(5.95)
+// console.log(f); // 5
+// GET THE: ceiling value (high)
+// var f = Math.ceil(5.95)
+// console.log(f);
+
+// * * * 84 Populating data to UI and add Tweet feature * * * 
+
+// * * * 85 Add clickable link to the news title * * * 
+
+// * * * 86 GO LIVE with GITHUB Pages * * * 
+// MY GitHub Page is:
+// https://codesigndesign.github.io/master-javascript-from-scratch-4jquery-and-1reactjs/. 
